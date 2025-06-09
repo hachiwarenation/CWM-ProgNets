2d51770f93fa42203c5c0d95087c5c029bc00992
hachiwarenation <nemomartin11@gmail.com>
2025-06-09 12:56:57 +0100

    A complete solution to Exercise 1

--- assignment1/send.py
+++ assignment1/send.py
@@ -1,3 +1,4 @@
+""" Edited from original to reflect changes required in Exercise 1 """
 #!/usr/bin/python
 
 from scapy.all import Ether, IP, sendp, get_if_hwaddr, get_if_list, TCP, Raw, UDP
@@ -10,19 +11,19 @@ def randomword(length):
 
 def send_random_traffic(num_packets, interface, src_ip, dst_ip):
     dst_mac = "00:00:00:00:00:01"
-    src_mac= "00:00:00:00:00:02"
+    src_mac= "CA:FE:CA:FE:CA:FE"
     total_pkts = 0
-    port = 1024
+    port = 5555
     for i in range(num_packets):
-            data = randomword(22)
+            data = randomword(458) #TCP has a header size 54 so we need 512-54=458B
             p = Ether(dst=dst_mac,src=src_mac)/IP(dst=dst_ip,src=src_ip)
-            p = p/UDP(sport= 50000, dport=port)/Raw(load=data)
+            p = p/TCP(sport= 50000, dport=port)/Raw(load=data)
             sendp(p, iface = interface, inter = 0.01)
             # If you want to see the contents of the packet, uncomment the line below
             # print(p.show())
             total_pkts += 1
     print("Sent %s packets in total" % total_pkts)
-
+ 	
 if __name__ == '__main__':
     if len(sys.argv) < 5:
         print("Usage: python send.py number_of_packets interface_name src_ip_address dst_ip_address")
