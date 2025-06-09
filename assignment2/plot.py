dbec518c0931b9f2c98c858d2fb232b8d25cb504
hachiwarenation <example@gmail.com>
2025-06-09 16:46:33 +0100

    Partially complete: Ping section done

--- assignment2/plot.py
+++ assignment2/plot.py
@@ -1,22 +1,28 @@
+""" idk tbh """
 # !/usr/bin/python3
 import numpy as np
 import matplotlib.pyplot as plt
 
-# parameters to modify
-filename="test.data"
-label='label'
-xlabel = 'xlabel'
-ylabel = 'ylabel'
-title='Simple plot'
-fig_name='test.png'
-bins=100 #adjust the number of bins to your plot
+
+# parameters to modify REMEMBER TO CHANGE WHEN SWAPPING TO/FROM CDF
+filenames=["processed_ping.txt"]
+labels=['i=0.01']
+xlabel = 'time'
+ylabel = 'proportion'
+title='RTT for each ping'
+fig_name='testc1.png'
+bins=1000 #adjust the number of bins to your plot
+
+for i in range(len(filenames)):
+    t = np.loadtxt(filenames[i], dtype="float")
+    #x = [p for p in range(1,len(t)+1)]
+
+    #plt.plot(t[:,0], t[:,1], label=labels[i])  # Plot some data on the (implicit) axes.
+
+    #Comment the line above and uncomment the line below to plot a CDF
+    plt.hist(t[:,1], bins, density=True, histtype='step', cumulative=True, label=labels[i])
 
 
-t = np.loadtxt(filename, delimiter=" ", dtype="float")
-
-plt.plot(t[:,0], t[:,1], label=label)  # Plot some data on the (implicit) axes.
-#Comment the line above and uncomment the line below to plot a CDF
-#plt.hist(t[:,1], bins, density=True, histtype='step', cumulative=True, label=label)
 plt.xlabel(xlabel)
 plt.ylabel(ylabel)
 plt.title(title)
